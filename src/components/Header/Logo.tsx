8202258ada3a8083887dde32047c276a9174b6ee
XiaoXice <liangxiao0991@gmail.com>
2023-04-10 21:54:17 +0800

    fix: 更换logo

--- src/components/Header/Logo.tsx
+++ src/components/Header/Logo.tsx
@@ -1,7 +1,7 @@
 import { Avatar } from 'antd';
 import React from 'react';
 
-import logo from '@/assets/logo.jpg';
+import logo from '@/assets/logo.svg';
 
 const Logo: React.FC<{ size: number }> = ({ size = 120 }) => (
   <Avatar size={size} src={logo} />
